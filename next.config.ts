9de16ab1ce76fcf61f6ad19fdee5c77d70ef554e
Lux Fero <aleks.luxfero@gmail.com>
2025-03-09 15:03:07 +0500

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["grammy", "@huggingface/inference"],
-  },
+  serverExternalPackages: ["grammy", "@huggingface/inference"],
 };
 
 export default nextConfig;
